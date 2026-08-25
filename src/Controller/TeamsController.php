46943d351a9b7dc1a74adc652cc79fdba7b70af0
Frank <office@falbrecht.de>
2026-08-25 22:08:13 +0200

    Update TeamsController.php

--- src/Controller/TeamsController.php
+++ src/Controller/TeamsController.php
@@ -165,7 +165,7 @@ class TeamsController extends AppController
         $year = $this->Cache->getCurrentYear()->toArray();
 
         $teams = $this->Teams->find('all', array(
-            'order' => array('hidden' => 'ASC', 'testTeam' => 'DESC', 'calcTotalRanking' => 'ASC', 'id' => 'ASC'),
+            'order' => array('hidden' => 'ASC', 'testTeam' => 'DESC', 'calcTotalRanking IS NULL ASC', 'calcTotalRanking' => 'ASC', 'id' => 'ASC'),
         ))->limit($year['teamsCount']);
 
         if ($teams->count() == 0) {
